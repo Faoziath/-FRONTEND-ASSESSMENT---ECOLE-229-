006b04079c262f39f9612410c7c766efd082a5a1
Faoziath ABOUDOU ALI <aboudoualifao@gmail.com>
2023-12-30 15:43:36 +0000

    AJOUT FICHER

---
+++ EXERCICE2/ex2.js
@@ -0,0 +1,7 @@
+let nom=String (prompt("Entrer votre nom"));
+let prenom=String (prompt("Entrer votre prenom"));
+let anne=Number (prompt("Entrer votre année de naissance"))
+function age (année)
+{let diff= 
+
+}
